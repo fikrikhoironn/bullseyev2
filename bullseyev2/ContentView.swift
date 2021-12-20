be8e6c8e335b0aa3fec29cd0be4d94a301f5eb00
fikrikhoironn <fikrikf470@gmail.com>
2021-12-21 06:08:48 +0700

    test commit

--- bullseyev2/ContentView.swift
+++ bullseyev2/ContentView.swift
@@ -1,21 +1,38 @@
 //
 //  ContentView.swift
-//  bullseyev2
+//  bullseye
 //
-//  Created by Fikri Khoiron Fadhila on 21/12/21.
-//
-
+//  Created by Fikri Khoiron Fadhila on 19/12/21.
+// test
+// fdsfsd
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+            Text("89")
+            HStack {
+                Text("1")
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+                    .kerning(2.0)
+                    .bold()
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Hit me")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
     }
 }
