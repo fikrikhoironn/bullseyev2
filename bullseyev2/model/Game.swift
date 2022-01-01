ed7f3b8a711c5d951f50e33d452519179ea68ec5
fikrikhoironn <fikrikf470@gmail.com>
2022-01-01 17:46:37 +0700

    bullseyev2 backup from bullseye

---
+++ bullseyev2/model/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  bullseyev2
+//
+//  Created by Fikri Khoiron Fadhila on 01/01/22.
+//
+
+import Foundation
+
+struct Game {
+    var target: Int = Int.random(in: 1...100)
+    var score: Int = 0
+    var round: Int = 1
+    func points(sliderValue: Int) -> Int
+    {
+        return 999
+    }
+}
---
+++ bullseyev2/view/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  bullseye
+//
+//  Created by Fikri Khoiron Fadhila on 19/12/21.
+// test
+// fdsfsd
+import SwiftUI
+
+struct ContentView: View {
+    @State private var alertIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
+    @State private var game: Game = Game()
+    
+    var body: some View {
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .kerning(2.0)
+                .bold()
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text(String(game.target))
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            HStack {
+                Text("1")
+                    .bold()
+                Slider(value: self.$sliderValue, in: 1.0...100.0)
+                Text("100")
+                    .bold()
+            }
+            Button(action: {
+                self.alertIsVisible = true
+            }) {
+                Text("Hit me")
+            }
+            .alert(isPresented: $alertIsVisible, content: {
+                var roundedValue: Int = Int(self.sliderValue.rounded())
+                return Alert(title: Text("Hello there!"), message: Text("This is \(roundedValue)\n" + "you scored \(self.game.points(sliderValue: roundedValue))") , dismissButton: .default(Text("awesome!")))
+            })
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
+    }
+}
